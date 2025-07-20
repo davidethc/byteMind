a6fb151590ca33ea1f59b9806b74c351fc8a760b
davidethc <davidecondet@gmail.com>
2025-07-20 15:28:49 -0500

    icono menu

--- monkyStartup/src/components/header.jsx
+++ monkyStartup/src/components/header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "boxicons/css/boxicons.min.css";
 
 const Header = () => {
   return (
@@ -10,8 +11,29 @@ const Header = () => {
         <a
           className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 "
           href="#"
-        ></a>
+        >
+          QuieneSomos
+        </a>
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 "
+          href="#"
+        >
+          Contacta
+        </a>
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50 "
+          href="#"
+        >
+          Contrata
+        </a>
       </nav>
+      <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor pointer scale-z-50">
+        ENTRAR
+      </button>
+      {/* mobile menu button - visible only on moblie */}
+      <button className="md:hidden text-3xl p-2 z-50">
+        <i className="bx bx-menu"></i>
+      </button>
     </header>
   );
 };
