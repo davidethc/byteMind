6ebfecf7a850a02f52634985f23a6d55a6f928fd
davidethc <davidecondet@gmail.com>
2025-07-20 16:27:12 -0500

    caja y gradiente

--- monkyStartup/src/components/hero.jsx
+++ monkyStartup/src/components/hero.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "boxicons/css/boxicons.min.css";
 
-const hero = () => {
+const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
       <div className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0 ">
-        {/* tag boox gradients border */}
-        <div>
-          <div>
-            <i class="bx bx-diamond">Introducing</i>
+        {/* tag box gradients border */}
+        <div className="relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.5)] rounded-full">
+          <div className="absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1">
+            <i className="bx bx-diamond"></i>
+            Introducing
           </div>
         </div>
       </div>
@@ -16,4 +17,4 @@ const hero = () => {
   );
 };
 
-export default hero;
+export default Hero;
