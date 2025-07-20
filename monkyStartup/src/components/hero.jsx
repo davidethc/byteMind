acead18f2e3e0a30eafb9b27579c9c62616af104
davidethc <davidecondet@gmail.com>
2025-07-20 16:18:35 -0500

    hero introducing card

--- monkyStartup/src/components/hero.jsx
+++ monkyStartup/src/components/hero.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import "boxicons/css/boxicons.min.css";
 
 const hero = () => {
   return (
-    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]"></main>
+    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
+      <div className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0 ">
+        {/* tag boox gradients border */}
+        <div>
+          <div>
+            <i class="bx bx-diamond">Introducing</i>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
